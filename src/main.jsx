94964154f640e213e5f7a540d148526e9b7092fe
unknown <mdtanvir7462@gmail.com>
2023-05-17 23:22:31 +0600

    lets start with vite tailwind daisyui and react router

---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+// -----------route setup-------
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+ <RouterProvider router={router} />
+  </React.StrictMode>,
+)
