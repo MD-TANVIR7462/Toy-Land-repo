5c3557629889278fcea1c601a435965674b679ec
unknown <mdtanvir7462@gmail.com>
2023-05-18 11:54:17 +0600

    benner added

--- src/components/Home/Benner/Benner.jsx
+++ src/components/Home/Benner/Benner.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 const Benner = () => {
    return (
       <div>
-         <div className="hero min-h-[650px] bg-base-200 ">
+         <div className="hero min-h-[650px] bg-base-100 ">
   <div className="hero-content flex-col lg:flex-row-reverse">
-    <img src="https://i.ibb.co/mFsNF1H/aoety-3zk39.jpg" className="max-w-2xl  rounded-lg shadow-3xl hero-overlay opacity-95" />
+    {/* <img src="https://i.ibb.co/mFsNF1H/aoety-3zk39.jpg" className="max-w-2xl  hover: hover:skew-y-2 hover:transition  hover:duration-700 hover:ease-in-out rounded-lg shadow-3xl hero-overlay opacity-95" /> */}
+    <img src="https://i.ibb.co/mFsNF1H/aoety-3zk39.jpg" className="max-w-2xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105  hover:skew-y-2 hover:transition  hover:duration-700 hover:ease-in-out  rounded-lg shadow-3xl hero-overlay opacity-95" />
     <div>
       <h1 className="text-5xl font-bold">BEST TOYS FOR YOUR KIDS!</h1>
       <p className="py-6">There’s just something about all of the different toys that makes me feel alive. After all, playing with toys is one of the best ways to spend my free time. We ended up buying a few things, but we didn’t buy anything very big or expensive. I thought that the prices were really fair for what you get. Overall, I had a great time at the toy store..</p>
-      <button className="btn btn-primary text-white">Visit Shop</button>
+      <button  class="transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 btn btn-primary border-none text-white ">Visit Shop</button>
     </div>
   </div>
 </div>
