dc5e38b7919fcbedc89d6e8b1962ce5951291355
unknown <mdtanvir7462@gmail.com>
2023-05-20 01:09:29 +0600

    some changes to the add a toy page

--- src/components/ADD a Toy/AddAToy.jsx
+++ src/components/ADD a Toy/AddAToy.jsx
@@ -1,132 +1,121 @@
 import React from 'react';
 
 const AddAToy = () => {
+
+
+   const handleADDaToy =(e)=>{
+      e.preventDefault()
+      const from = e.target
+      const  toy_name = from.name.value
+      const seller_name = from.sellerName.value
+      const sellerEmail = from.sellerEmail.value
+      const sub_category = from.subCategory.value
+      const price = from.price.value
+      const rating = from.rating.value
+      const available_quantity = from.availableQuantity.value
+      const description = from.description.value
+      const picture = from.img.value
+      
+   
+     
+   }
+   
+
+
+
   return (
-    <div className="  p-16 min-h-screen flex justify-center items-center px-4 sm:px-8">
-      <div className="max-w-xl w-full  bg-white shadow-lg  rounded-lg overflow-hidden">
-        <div className="py-4 px-6">
-          <h1 className="text-3xl font-bold text-center text-primary mb-6">Add A Toy</h1>
+   <div className="hero min-h-screen  bg-zinc-100">
+   <div className="hero-content flex-col ">
+     <div className="text-center ">
+       <h1 className="text-5xl text-black font-bold">Add A Toy!</h1>
+     </div>
+     <form  onSubmit={handleADDaToy} className="card  md:w-[450px] shadow-2xl bg-base-100" >
+       <div className="card-body">
+         <div className="form-control">
+           <label className="label">
+             <span className="label-text"> Toy Name</span>
+           </label>
+           <input type="text" name='name' required placeholder="Toy Name" className="input input-bordered" />
+         </div>
+         <div className="form-control">
+           <label className="label">
+             <span className="label-text">Toy URL</span>
+           </label>
+           <input type="url" name='img' required placeholder="Toy URL" className="input input-bordered" />
+         </div>
+         <div className="form-control">
+           <label className="label">
+             <span className="label-text">sellerEmail</span>
+           </label>
+           <input type="email"  name='sellerEmail' required placeholder="sellerEmail" className="input input-bordered" />
+         </div>
 
-          <div className="mb-4">
-            <label htmlFor="pictureUrl" className="block text-gray-700 font-semibold mb-2">
-              Picture URL of the toy
-            </label>
-            <input
-              type="text"
-              id="pictureUrl"
-              className="border border-gray-300 rounded px-4 py-2 w-full focus:outline-none focus:border-blue-500"
-              required
-            />
-          </div>
 
-          <div className="mb-4">
-            <label htmlFor="name" className="block text-gray-700 font-semibold mb-2">
-              Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              className="border border-gray-300 rounded px-4 py-2 w-full focus:outline-none focus:border-blue-500"
-              required
-            />
-          </div>
-
-          <div className="mb-4">
-            <label htmlFor="sellerName" className="block text-gray-700 font-semibold mb-2">
-              Seller Name
-            </label>
-            <input
-              type="text"
-              id="sellerName"
-              className="border border-gray-300 rounded px-4 py-2 w-full focus:outline-none focus:border-blue-500"
-              required
-            />
-          </div>
-
-          <div className="mb-4">
-            <label htmlFor="sellerEmail" className="block text-gray-700 font-semibold mb-2">
-              Seller Email
-            </label>
-            <input
-              type="email"
-              id="sellerEmail"
-              className="border border-gray-300 rounded px-4 py-2 w-full focus:outline-none focus:border-blue-500"
-              required
-            />
-          </div>
-
-          <div className="mb-4">
-            <label htmlFor="subCategory" className="block text-gray-700 font-semibold mb-2">
-              Sub-category
-            </label>
-            <select
-              id="subCategory"
-              className="border border-gray-300 rounded px-4 py-2 w-full focus:outline-none focus:border-blue-500"
+         <div className="form-control">
+           <label className="label">
+             <span className="label-text">sellerName</span>
+           </label>
+           <input type="text" required placeholder="sellerName" name='sellerName' className="input input-bordered" />
+          
+         </div>
+         <div className="form-control">
+           <label className="label">
+             <span className="label-text">subCategory</span>
+           </label>
+           <select
+              name="subCategory"
+              className="input input-bordered"
+              placeholder='select Subcetegory'
               required
             >
-              <option value="">Select Sub-category</option>
-              <option value="math">Dinosaur</option>
-              <option value="language">Teddy Bear</option>
-              <option value="science">Butterfly</option>
+              <option value="">Change ME</option>
+              <option value="Dinosaur">Dinosaur</option>
+              <option value="Butterfly">Butterfly</option>
+              <option value="Teddy Bear">Teddy Bear</option>
             </select>
-          </div>
+          
+         </div>
 
-          <div className="mb-4">
-            <label htmlFor="price" className="block text-gray-700 font-semibold mb-2">
-              Price
-            </label>
-            <input
-              type="number"
-              id="price"
-              className="border border-gray-300 rounded px-4 py-2 w-full focus:outline-none focus:border-blue-500"
-              required
-            />
-          </div>
+         <div className="form-control">
+           <label className="label">
+             <span className="label-text">price</span>
+           </label>
+           <input type="number" required placeholder="price" name='price' className="input input-bordered" />
+          
+         </div>
+         <div className="form-control">
+           <label className="label">
+             <span className="label-text">rating</span>
+           </label>
+           <input type="number" required placeholder="rating" name='rating' className="input input-bordered" />
+          
+         </div>
+         <div className="form-control">
+           <label className="label">
+             <span className="label-text">availableQuantity</span>
+           </label>
+           <input type="number" required placeholder="availableQuantity" name='availableQuantity' className="input input-bordered" />
+          
+         </div>
+         <div className="form-control">
+           <label className="label">
+             <span className="label-text">description </span>
+           </label>
+           <input type="text" required placeholder="description " name='description' className="input input-bordered" />
+          
+         </div>
+        
 
-          <div className="mb-4">
-            <label htmlFor="rating" className="block text-gray-700 font-semibold mb-2">
-              Rating
-            </label>
-            <input
-              type="number"
-              id="rating"
-              className="border border-gray-300 rounded px-4 py-2 w-full focus:outline-none focus:border-blue-500"
-              required
-            />
-          </div>
 
-          <div className="mb-4">
-            <label htmlFor="availableQuantity" className="block text-gray-700 font-semibold mb-2">
-              Available Quantity
-            </label>
-            <input
-              type="number"
-              id="availableQuantity"
-              className="border border-gray-300 rounded px-4 py-2 w-full focus:outline-none focus:border-blue-500"
-              required
-            />
-          </div>
 
-          <div className="mb-4">
-            <label htmlFor="description" className="block text-gray-700 font-semibold mb-2">
-              Detail Description
-            </label>
-            <textarea
-              id="description"
-              className="border border-gray-300 rounded px-4 py-2 w-full h-24 resize-none focus:outline-none focus:border-blue-500"
-              required
-            ></textarea>
-          </div>
-
-          <button
-            type="submit"
-            className="btn btn-primary w-full text-white"
-          >
-            Add A Toy
-          </button>
-        </div>
-      </div>
-    </div>
+         <div className="form-control mt-6">
+         
+           <button className="btn btn-primary">Add A Toy</button>
+         </div>
+       </div>
+     </form >
+   </div>
+ </div>
   );
 };
 
