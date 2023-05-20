2d9437d09e3fbc291585726a0b35b223850547c1
unknown <mdtanvir7462@gmail.com>
2023-05-21 00:34:20 +0600

    delete mathood done

--- src/components/MyToys/Mytoys.jsx
+++ src/components/MyToys/Mytoys.jsx
@@ -1,57 +1,92 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../providers/AuthProvider';
 import SingleTOy from './SingleTOy';
+import Swal from 'sweetalert2';
 
 const Mytoys = () => {
-const [Userdata,setedata] = useState(null)
- 
-const {isLoggedIn} = useContext(AuthContext)
+   const [Userdata, setedata] = useState(null)
+
+   const { isLoggedIn } = useContext(AuthContext)
 
 
-   console.log(isLoggedIn.email)
 
-useEffect(()=>{
-   fetch(`https://assignment-11-server-blue-rho.vercel.app/singleEmail?sellerEmail=${isLoggedIn.email}`)
-   .then(res=>res.json())
-   .then(data=>setproduct(data))
-},[])
-const setproduct = (data)=>{
-if(data.length>0){
-   setedata(data)
-}
-else{
-   setedata(null)
-}
-}
+
+   const deletHandle = (id) =>{
+      Swal.fire({
+       title: 'Are you sure?',
+       text: "You won't to Detel it???",
+       icon: 'warning',
+       showCancelButton: true,
+       confirmButtonColor: '#3085d6',
+       cancelButtonColor: '#d33',
+       confirmButtonText: 'Yes, delete it!'
+     }).then((result) => {
+       if (result.isConfirmed) {
+      
+       fetch(`http://localhost:5000/toyland/${id}`,{
+          method:"delete"
+       })
+       .then(res=>res.json())
+       .then(data=>{
+          
+          console.log(data)})
+       Swal.fire(
+           'Deleted!',
+           'Your Product has been deleted.',
+           'success'
+         )
+
+       const remainingProduct = Userdata.filter(product=> product._id !== id)
+       setedata(remainingProduct)
+       }
+     })
+    
+    
+    }
+
+
+   useEffect(() => {
+      fetch(`https://assignment-11-server-blue-rho.vercel.app/singleEmail?sellerEmail=${isLoggedIn.email}`)
+         .then(res => res.json())
+         .then(data => setproduct(data))
+   }, [])
+   const setproduct = (data) => {
+      if (data.length > 0) {
+         setedata(data)
+      }
+      else {
+         setedata(null)
+      }
+   }
 
    return (
       <div className="overflow-x-auto w-full">
- {
-   Userdata? <table className="table w-full ">
-   {/* head */}
-   <thead>
-     <tr className='mt-5'>
-     <th className='pl-16 '>Toy Name</th>
-                    <th>Seller Company Name </th>
-                    <th>Sub-Category</th>
-                    <th>Price</th>
-                    <th>Available </th>
-                    <th className='pr-0'>Update/Delet</th>
-     </tr>
-   </thead>
-  
-    
-   {
-     Userdata.map(singleData=><SingleTOy singleData={singleData} key={singleData._id}></SingleTOy>)
-   }
- 
-   
- </table>:
- <p className='text-primary text-4xl items-center text-center min-h-[500px] pt-32
- '>You Don't Add any Product Yet!!  </p>
- }
+         {
+            Userdata ? <table className="table w-full ">
+               {/* head */}
+               <thead>
+                  <tr className='mt-5'>
+                     <th className='pl-16 '>Toy Name</th>
+                     <th>Seller Company Name </th>
+                     <th>Sub-Category</th>
+                     <th>Price</th>
+                     <th>Available </th>
+                     <th className='pr-0'>Update/Delet</th>
+                  </tr>
+               </thead>
 
-</div>
+
+               {
+                  Userdata.map(singleData => <SingleTOy singleData={singleData} deletHandle={deletHandle} key={singleData._id}></SingleTOy>)
+               }
+
+
+            </table> :
+               <p className='text-primary text-4xl items-center text-center min-h-[500px] pt-32
+ '>You Don't Add any Product Yet!!  </p>
+         }
+
+      </div>
 
    );
 };
--- src/components/MyToys/SingleTOy.jsx
+++ src/components/MyToys/SingleTOy.jsx
@@ -1,6 +1,40 @@
 import React from 'react';
+import Swal from 'sweetalert2';
 
-const SingleTOy = ({ singleData }) => {
+const SingleTOy = ({ singleData,deletHandle }) => {
+
+
+// const deletHandle = (id) =>{
+//   Swal.fire({
+//    title: 'Are you sure?',
+//    text: "You won't to Detel it???",
+//    icon: 'warning',
+//    showCancelButton: true,
+//    confirmButtonColor: '#3085d6',
+//    cancelButtonColor: '#d33',
+//    confirmButtonText: 'Yes, delete it!'
+//  }).then((result) => {
+//    if (result.isConfirmed) {
+  
+//    fetch(`http://localhost:5000/toyland/${id}`,{
+//       method:"delete"
+//    })
+//    .then(res=>res.json())
+//    .then(data=>{
+      
+//       console.log(data)})
+//    Swal.fire(
+//        'Deleted!',
+//        'Your Product has been deleted.',
+//        'success'
+//      )
+   
+//    }
+//  })
+
+
+// }
+
    const {
       picture,
       email,
@@ -36,7 +70,7 @@ const SingleTOy = ({ singleData }) => {
             <td className='text-white'>{available_quantity} Items</td>
             <th className='ps-5'>
                <div className='flex flex-col my-2'>
-                  <button className="btn btn-primary text-white font-bold btn-xs bg-success hover:bg-red-700 transition-all">Delete</button>
+                  <button className="btn btn-primary text-white font-bold btn-xs bg-success hover:bg-red-700 transition-all" onClick={()=>deletHandle(_id)}>Delete</button>
                   <button className="btn btn-primary text-white font-bold btn-xs mt-3 bg-primary hover:bg-blue-700 transition-all">Update</button>
                </div>
             </th>
